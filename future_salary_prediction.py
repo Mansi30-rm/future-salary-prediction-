54fb4070373482a77bd54b35d7270357e8a1d979
Mansi Lal <mansi180430@gmail.com>
2025-07-23 10:23:22 +0530

    Update future_salary_prediction.py

--- future_salary_prediction.py
+++ future_salary_prediction.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-# Load the dataset
+# Download dataset from: https://www.kaggle.com/dataset-survey_results_public.csv
 df = pd.read_csv("survey_results_public.csv", low_memory=False)
 
 # Rename columns for simplicity
